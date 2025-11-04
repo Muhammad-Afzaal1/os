03bb1f4cdb9078bb2de4cf80c2bbdf38c2c698a8
Muhammad-Afzaal1 <afzaal4517muhammad@gmail.com>
2025-11-04 11:43:20 +0500

    random number using time.h and srand

--- random.c
+++ random.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
+    srand(time(NULL));
+    int random_number1 = rand();
+    int random_number2 = rand();
     int num = rand();
-    printf("Random number is: %d", num);
+    printf("Random number 1 : %d\n", random_number1);
+    printf("Random number 2: %d\n", random_number2);
     return 0;
 }
